578f14963db1d8e8b2ee9580248ee97f3676d7a1
Anders Planting <anders@andersplanting.se>
2010-12-12 10:04:46 +0100

    Activated sphinx

--- cookbooks/sphinx/recipes/default.rb
+++ cookbooks/sphinx/recipes/default.rb
@@ -4,10 +4,10 @@
 #
 
 # Set your application name here
-appname = "appname"
+appname = "Trenchy2"
 
 # Uncomment the flavor of sphinx you want to use
-#flavor = "thinking_sphinx"
+flavor = "thinking_sphinx"
 #flavor = "ultrasphinx"
 
 # If you want to install on a specific utility instance rather than
